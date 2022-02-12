fa14856978e33a286d515757d862786bd2a0971b
Antoni Kiszka <antoni@kiszka.ovh>
2022-02-12 21:54:55 +0100

    Basic rust notify

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,50 @@
-use rocket::serde::{Deserialize, json::Json};
+#![feature(bool_to_option)]
+use std::process::Command;
+use rocket::{http::Status, form::{Form, self}};
 #[macro_use] extern crate rocket;
 
-#[derive(Deserialize)]
+#[derive(FromForm)]
 struct Message<'a> {
-    text: &'a str
+    to_group: bool,
+    #[field(validate = validate_recipient())]
+    recipient: &'a str,
+    text: &'a str,
+    #[field(validate = eq("TFu27M4a7lKXq33FHBihepP3XgSZTi7maTBARVxr"))]
+    #[field(name = "key")]
+    _key: &'a str
+}
+
+fn validate_recipient<'v>(value: &str) -> form::Result<'v, ()> {
+    if value.chars().all(|c| c.is_ascii_alphanumeric() || c == '+' || c == '/' || c == '=') {
+        Ok(())
+    } else {
+        Err(form::Error::validation("Bad characters in recipient"))?
+    }
 }
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Welcome to the Parlour Signal Bot!\nhttps://parlour.dev"
 }
 
 #[post("/notify", data = "<message>")]
-fn send(message: Json<Message<'_>>) -> &'_ str {
-    message.text
+fn send(message: Form<Message<'_>>) -> Result<&'_ str, Status> {
+    let args = if message.to_group {
+        vec!["--dbus", "send", "-m", message.text, "-g", message.recipient]
+    } else {
+        vec!["--dbus", "send", "-m", message.text, message.recipient]
+    };
+
+    Command::new("signal-cli")
+        .args(&args)
+        .status()
+        .map_or(Err(Status::InternalServerError), |status| {
+            if status.success() {
+                Ok("sent")
+            } else {
+                Err(Status::InternalServerError)
+            }
+        })
 }
 
 
