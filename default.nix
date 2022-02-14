386e5a73ba1199130cd63773535a3293a5ecf0ab
Antoni Kiszka <antoni@kiszka.ovh>
2022-02-14 21:25:52 +0100

    Remove the need for /data in the container

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ let
 in pkgs.dockerTools.buildLayeredImage {
   inherit name tag;
   created = "now";
-  contents = [ signalbot pkgs.signal-cli ./additional ./data ];
+  contents = [ signalbot pkgs.signal-cli ./additional ];
 
   config = {
     Cmd = [ "/bin/bot" ];
