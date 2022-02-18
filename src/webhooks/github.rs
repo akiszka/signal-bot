e76c09ab649b199ae96a5e1ba43bb70b84e5d2da
Antoni Kiszka <antoni@kiszka.ovh>
2022-02-18 18:10:25 +0100

    Change to &str

--- src/webhooks/github.rs
+++ src/webhooks/github.rs
@@ -2,70 +2,71 @@ use super::WebhookPayload;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Payload {
+pub struct Payload<'a> {
     #[serde(rename = "ref")]
-    ref_on: Option<String>,
-    before: Option<String>,
-    after: Option<String>,
+    ref_on: Option<&'a str>,
+    before: Option<&'a str>,
+    after: Option<&'a str>,
 
-    commits: Option<Vec<Commit>>,
-    head_commit: Option<Commit>,
+    commits: Option<Vec<Commit<'a>>>,
+    head_commit: Option<Commit<'a>>,
 
-    action: Option<String>,
-    repository: Repository,
-    sender: User,
+    action: Option<&'a str>,
+    repository: Repository<'a>,
+    sender: User<'a>,
 
-    issue: Option<Issue>,
-    pull_request: Option<Issue>,
+    issue: Option<Issue<'a>>,
+    pull_request: Option<Issue<'a>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct User {
-    login: String,
+struct User<'a> {
+    login: &'a str,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Repository {
-    name: String,
-    full_name: String,
-    url: String,
-    html_url: String,
+struct Repository<'a> {
+    name: &'a str,
+    full_name: &'a str,
+    url: &'a str,
+    html_url: &'a str,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Issue {
-    url: String,
-    html_url: String,
+struct Issue<'a> {
+    url: &'a str,
+    html_url: &'a str,
     id: u64,
-    title: String,
+    title: &'a str,
     number: i64,
-    user: User,
-    state: String,
+    user: User<'a>,
+    state: &'a str,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Commit {
-    id: String,
-    message: String,
-    url: String,
+struct Commit<'a> {
+    id: &'a str,
+    message: &'a str,
+    url: &'a str,
 }
 
-impl WebhookPayload for Payload {
+impl WebhookPayload for Payload<'_> {
     fn to_string(&self) -> String {
         let mut message = String::new();
-        message.push_str("[GitHub]: ");
-        message.push_str(self.sender.login.as_str());
+        message.push_str(self.repository.full_name);
+        message.push_str(": ");
+        message.push_str(self.sender.login);
         match &self.action {
             Some(action) => {
                 message.push(' ');
-                message.push_str(action.as_str());
+                message.push_str(action);
 
                 match &self.issue {
                     Some(issue) => {
                         message.push_str(" issue '");
-                        message.push_str(issue.title.as_str());
+                        message.push_str(issue.title);
                         message.push_str("'\n");
-                        message.push_str(issue.html_url.as_str());
+                        message.push_str(issue.html_url);
                     }
                     None => {}
                 }
@@ -73,9 +74,9 @@ impl WebhookPayload for Payload {
                 match &self.pull_request {
                     Some(pull_request) => {
                         message.push_str(" pull request '");
-                        message.push_str(pull_request.title.as_str());
+                        message.push_str(pull_request.title);
                         message.push_str("'\n");
-                        message.push_str(pull_request.html_url.as_str());
+                        message.push_str(pull_request.html_url);
                     }
                     None => {}
                 }
@@ -86,23 +87,17 @@ impl WebhookPayload for Payload {
         match &self.ref_on {
             Some(ref_on) => {
                 let branch_name = ref_on
-                    .as_str()
                     .split('/')
                     .nth(2)
                     .map_or("".to_string(), |s| "branch ".to_string() + s); // remove "refs/heads/"
 
-                if self.after.as_deref() == Some("0000000000000000000000000000000000000000") {
+                if self.after == Some("0000000000000000000000000000000000000000") {
                     message.push_str(" deleted ");
-                } else if self.before.as_deref() == Some("0000000000000000000000000000000000000000")
-                {
+                } else if self.before == Some("0000000000000000000000000000000000000000") {
                     message.push_str(" created ");
                 } else if self.head_commit.is_some() {
                     message.push_str(" commited ");
-                    let commit_name = self
-                        .head_commit
-                        .as_ref()
-                        .map(|c| c.message.as_str())
-                        .unwrap_or("");
+                    let commit_name = self.head_commit.as_ref().map(|c| c.message).unwrap_or("");
                     message.push_str(("'".to_string() + commit_name + "'").as_str());
                     message.push_str(" to ");
                 }
